8cd37399ce2ef20bfddd5396198f409ab97ed08d
Belei-Vitalii-Student <99961129+Belei-Vitalii-Student@users.noreply.github.com>
2024-02-28 11:21:31 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import math
+
+var_int = 10
+var_float = 8.4
+var_str = "No"
+
+big_int = var_int * 3.5
+
+var_float -= 1
+
+var_int / var_float
+big_int / var_float
+
+var_str += "No" + "Yes" * 3
+
+
+# Main task
+
+c = 1
+x = 2
+
+a = -1
+b = -2
+
+result = 5 * math.sqrt(c * (x ** 3) + 1) + (a ** 3) - (b ** 2)
+
+print(f"c = {c}, x = {x}, a = {a}, b = {b}")
+print(result)
